a0ecdc09fc73dd3ca66cea3c875bf22d923f5a21
Alex Manaila <amanaila@oakland.edu>
2019-08-02 01:39:22 -0400

    created info window, modified info text

--- src/syminical/tetris_sprint/Tetris_Sprint.java
+++ src/syminical/tetris_sprint/Tetris_Sprint.java
@@ -11,7 +11,7 @@ public class Tetris_Sprint {
     
     public static Tetris_Sprint Instance;
     private WindowBox MainBox, SettingsBox, InteractiveBox, DevBox;
-	private final Dimension MAIN_BOX_SIZE = new Dimension( 251, 501 ), SETTINGS_BOX_SIZE = new Dimension( 600, 600 ), INFO_BOX_SIZE = new Dimension( 300, 100 );
+	private final Dimension MAIN_BOX_SIZE = new Dimension( 251, 501 ), SETTINGS_BOX_SIZE = new Dimension( 350, 300 ), INFO_BOX_SIZE = new Dimension( 300, 100 );
     
     public Tetris_Sprint() {
         createWindows();
@@ -56,10 +56,14 @@ public class Tetris_Sprint {
             }
         };
         
+        //game canvas
         MainBox.addComponent( new JPanel() {            
             public void paint(Graphics g) {
                 super.paint(g);
+                
                 this.setBackground(Color.BLACK);
+                
+                //click me button
                 this.setOpaque(true);
                 g.setColor(new Color(200, 200, 200));
                 g.fillRoundRect( (47), (222), (155), (57), 40, 40 );
@@ -71,11 +75,34 @@ public class Tetris_Sprint {
                 g.setFont(new Font("Comic Sans MS", Font.BOLD, 32));
                 g.drawString("Click Me", (25 * 3 - 16), (261));
                 
+                //information message button
                 g.setFont(new Font("Comic Sans MS", Font.BOLD, 12)); 
                 g.setColor(Color.WHITE);
                 g.drawString("press i for more information", 46, 480);
             }
         });
+        
+        //info box
+		SettingsBox = new WindowBox<Tetris_Sprint>(this, "Tetris - Sprint Info", SETTINGS_BOX_SIZE, null, null) {  
+            public void buildBox() {
+                //window customization
+                //this.setDefaultCloseOperation( WindowConstants.DO_NOTHING_ON_CLOSE );
+                //this.setResizable( false );
+                //this.setBackground( new Color( 0, 0, 0, 0 ) );
+                JEditorPane JEP = new JEditorPane();
+                JEP.setBackground(Color.BLACK);
+                JEP.setContentType("text/html");
+                JEP.setText("<p style = 'text-align: center;color: white;font-size: 15px;'><b>TETRIS - SPRINT<br>syminical 2019<br><br>* Right / Left arrow keys to move<br>* Down / Space / wait to go down<br>* Up to turn<br>* C to swap shapes<br>* R to restart<br><br>Clear 20 lines as fast as you can!<br><br><br></b></p>");
+                JEP.setFont( new Font("Comic Sans MS", Font.BOLD, 20) );
+                JEP.setEditable(false);
+                JEP.setHighlighter(null);
+                JEP.setOpaque(true);
+                this.addComponent( JEP );
+            }
+            
+            //Object Messaging, to trigger custom 'methods'.  
+            public void tell(int n) {}
+        };
     }
     
     public static void main(String[] Args) {
