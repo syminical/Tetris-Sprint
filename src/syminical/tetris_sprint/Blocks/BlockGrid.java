f82e991a3cf749b480584fef1270fc43185d7b24
Alex Manaila <amanaila@oakland.edu>
2020-01-31 17:43:38 -0500

    fixed fastdrop bug, need to fix clear row peak bug

--- src/syminical/tetris_sprint/Blocks/BlockGrid.java
+++ src/syminical/tetris_sprint/Blocks/BlockGrid.java
@@ -2,6 +2,7 @@ package syminical.tetris_sprint;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.ListIterator;
 import java.awt.Point;
 
 public class BlockGrid {
@@ -11,7 +12,7 @@ public class BlockGrid {
     private final int WIDTH = 10;
     private int size = 0;
     private int rowsCleared = 0;
-    private ArrayList<Integer> clearBuffer = new ArrayList<Integer>();
+    private ArrayList<Integer> ClearBuffer = new ArrayList<Integer>();
     private BlockType[][] grid = new BlockType[HEIGHT][WIDTH];
     private int[] peaks = new int[WIDTH];
     
@@ -30,13 +31,17 @@ public class BlockGrid {
     
     public Point fastDrop(Block B) {
         if (B == null) return new Point(0, 0);
-        int answer = HEIGHT;
+        int answer = HEIGHT-B.height();
         
         for (int i = 0; i < B.width(); ++i)
-            if (peaks[B.x()+i] < answer)
-                answer = peaks[B.x()+i];
+            for (int j = B.height()-1; j > -1; --j)
+                if (B.data()[B.state()][j][i]) {
+                    if (peaks[B.x()+i]-j-1 < answer)
+                        answer = peaks[B.x()+i]-j-1;
+                    break;
+                }
         
-        return new Point(B.x(), answer-B.height());
+        return new Point(B.x(), answer);
     }
     
     public boolean detectDown(Block B) {
@@ -45,7 +50,7 @@ public class BlockGrid {
         
         for (int i = B.height()-1; i > -1; --i)
             for (int j = 0; j < B.width(); ++j)
-                if (B.data()[B.state][i][j])
+                if (B.data()[B.state()][i][j])
                     if (grid[B.y()+i+1][B.x()+j] != null)
                         return true;
         return false;
@@ -91,7 +96,7 @@ public class BlockGrid {
         boolean success = true;
         B.turnRight();
         
-        for (int i = B.height(); i > -1; --i)
+        for (int i = B.height()-1; i > -1; --i)
             for (int j = B.width()-1; j > -1; --j)
                 if (B.data()[B.state()][i][j])
                     if(grid[B.y()+i][B.x()+j] != null) {
@@ -104,8 +109,8 @@ public class BlockGrid {
 		//ActiveBlock.clearShape(grid, 1);
 		int precision = 0;
 		
-		while (clearBuffer.size() > 0) {
-			for (int i = clearBuffer.get(0) + precision; i > 3; i--)
+		while (ClearBuffer.size() > 0) {
+			for (int i = ClearBuffer.get(0) + precision; i > 3; i--)
 				//for (int j = 0; j < WIDTH; ++j)
 					grid[i] = grid[i - 1];
 
@@ -114,15 +119,19 @@ public class BlockGrid {
                 grid[4] = new BlockType[WIDTH];
 
 			++precision;
-			clearBuffer.remove(0);
+			ClearBuffer.remove(0);
 		}
 
 		if (rowsCleared >= PARENT.rowsCap) PARENT.goalReached();
 	}
     
     private void clearRows() {
-		for (Integer container : clearBuffer) {
-			grid[container] = new BlockType[WIDTH];            
+        Integer __;
+		for (ListIterator<Integer> Itr = ClearBuffer.listIterator(ClearBuffer.size()); Itr.hasPrevious();) {
+            __ = Itr.previous();
+            for (int i = 0; i < WIDTH; ++i)
+                if (peaks[i] <= __) peaks[i] = peaks[i] + 1;
+			Arrays.fill(grid[__], null);//grid[__] = new BlockType[WIDTH];
 			++rowsCleared;
 		}
 		deFrag();
@@ -136,9 +145,9 @@ public class BlockGrid {
 				if (grid[i][j] == null) {
 					full = false; break;
                 }
-			if (full) clearBuffer.add(i);
+			if (full) ClearBuffer.add(i);
 		}
-        if (clearBuffer.size() > 0) clearRows();
+        if (ClearBuffer.size() > 0) clearRows();
 	}
     
     public int HIDDEN_HEIGHT() { return HIDDEN_HEIGHT; }
@@ -147,8 +156,9 @@ public class BlockGrid {
     public int rowsCleared() { return rowsCleared; }
     public int size() { return size; }
     public BlockType[][] data() { return grid; }
-    public void clear() { grid = new BlockType[HEIGHT + HIDDEN_HEIGHT][WIDTH]; clearBuffer.clear(); }
-    public void reset() { clear(); rowsCleared = 0; }
+    public int[] peaks() { return peaks; }
+    public void clear() { grid = new BlockType[HEIGHT + HIDDEN_HEIGHT][WIDTH]; ClearBuffer.clear(); }
+    public void reset() { clear(); rowsCleared = 0; Arrays.fill(peaks, 24); }
     public String toString() {
         String __ = "";    
 
